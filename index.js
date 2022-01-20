5cda3c6d7c603c7b73253a7c4b7743d499f04848
EvgeniyTTI <evgeniytti@rambler.ru>
2022-01-21 00:12:36 +0300

    test CRUD for user

--- index.js
+++ index.js
@@ -47,19 +47,54 @@ app.post('/signup', jsonParser, async (request, response)=> {
   }     
 });
 app.get('/login', jsonParser, async (request, response)=> {
-  const { authorization, fullName } = request.body;
-  const allUsers = await User.findAll({ where:{ fullName: fullName } });
+  const { fullName } = request.body;
+  const { authorization } = request.headers;
+  const token = authorization.split(' ')[1];
+  if (!token) {
+    response.status(403).json({message: "Пользователь не авторизован"})  
+  } else {  
+  const verifyResult = jwt.verify(token, secret);
+  if (!verifyResult) {
+    response.status(403).json({message: "Ошибка авторизации"})  
+  } else {  
+  const allUsers = await User.findAll({ where:{ fullName: verifyResult.userData } });
   response.send(allUsers);
+  }
+  }
 });
 app.put('/update', jsonParser, async (request, response)=> {
-  const { authorization, fullName } = request.body;
-  const allUsers = await User.findAll({ where:{ fullName: fullName } });
-  response.send(allUsers);
+  const { updateName } = request.body;
+  const { authorization } = request.headers;
+  const token = authorization.split(' ')[1];
+  if (!token) {
+    response.status(403).json({message: "Пользователь не авторизован"})  
+  } else {  
+  const verifyResult = jwt.verify(token, secret);
+  if (!verifyResult) {
+    response.status(403).json({message: "Ошибка авторизации"})  
+  } else { 
+  let test = await User.findAll( {where: { fullName: verifyResult.userData }} );
+  test.fullName = updateName;
+  const newUser = await User.update( test, {where: { fullName: verifyResult.userData }} );
+  response.send({ result: newUser });
+  }
+  }
 });
 app.delete('/delete', jsonParser, async (request, response)=> {
-  const { authorization, fullName } = request.body; 
-  const allUsers = await User.findAll({ where:{ fullName: fullName } });
-  response.send(allUsers);
+  const { updateName } = request.body;
+  const { authorization } = request.headers;
+  const token = authorization.split(' ')[1];
+  if (!token) {
+    response.status(403).json({message: "Пользователь не авторизован"})  
+  } else {  
+  const verifyResult = jwt.verify(token, secret);
+  if (!verifyResult) {
+    response.status(403).json({message: "Ошибка авторизации"})  
+  } else {  
+  await User.destroy( {where:{ fullName: updateName }} );
+  response.status(200).json({message: "Пользователь удален"})  
+  }
+  }
 });
 app.listen(3000, ()=> {
   console.log('сервер запущен')
