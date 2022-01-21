6015b1c2f10a1e852c51b35b7dace6c0fe7f177a
EvgeniyTTI <evgeniytti@rambler.ru>
2022-01-21 10:45:01 +0300

    andLogic withOut routes/midleware

--- index.js
+++ index.js
@@ -34,32 +34,33 @@ app.post('/signup', jsonParser, async (request, response)=> {
   if (isRegistred.length>0) {
     response.status(400).json({message: "Пользователь с таким именем уже зарегистрирован"})
   } else {   
-  const hasPassword = CryptoJS.AES.encrypt(hassMess, password).toString();  
-  const newUser = await User.create({
-  fullName: fullName,
-  email: email,
-  password: hasPassword,
-  dob: dob,
-  }).then(res=>{
-  }).catch(err=>console.log(err));  
-  const token = jwt.sign({ userData: fullName }, secret);
-  response.json({ token: token });
-  }     
+      const hasPassword = CryptoJS.AES.encrypt(hassMess, password).toString();  
+      const newUser = await User.create({
+      fullName: fullName,
+      email: email,
+      password: hasPassword,
+      dob: dob,
+      }).then(res=> {
+      console.log(res.dataValues.id);
+      const token = jwt.sign({ userData: res.dataValues.id }, secret);
+      response.json({ token: token });
+      }).catch(err=>console.log(err));   
+    }     
 });
-app.get('/login', jsonParser, async (request, response)=> {
-  const { fullName } = request.body;
+app.get('/login', jsonParser, async (request, response)=> {  
   const { authorization } = request.headers;
   const token = authorization.split(' ')[1];
   if (!token) {
     response.status(403).json({message: "Пользователь не авторизован"})  
   } else {  
-  const verifyResult = jwt.verify(token, secret);
-  if (!verifyResult) {
-    response.status(403).json({message: "Ошибка авторизации"})  
-  } else {  
-  const allUsers = await User.findAll({ where:{ fullName: verifyResult.userData } });
-  response.send(allUsers);
-  }
+    const verifyResult = jwt.verify(token, secret);
+    if (!verifyResult) {
+      response.status(403).json({message: "Ошибка авторизации"})  
+    } else {  
+      console.log(verifyResult.userData);
+      const allUsers = await User.findAll({ where:{ id: verifyResult.userData } });
+      response.send(allUsers);
+    }
   }
 });
 app.put('/update', jsonParser, async (request, response)=> {
@@ -69,32 +70,32 @@ app.put('/update', jsonParser, async (request, response)=> {
   if (!token) {
     response.status(403).json({message: "Пользователь не авторизован"})  
   } else {  
-  const verifyResult = jwt.verify(token, secret);
-  if (!verifyResult) {
-    response.status(403).json({message: "Ошибка авторизации"})  
-  } else { 
-  let test = await User.findAll( {where: { fullName: verifyResult.userData }} );
-  test.fullName = updateName;
-  const newUser = await User.update( test, {where: { fullName: verifyResult.userData }} );
-  response.send({ result: newUser });
-  }
+    const verifyResult = jwt.verify(token, secret);
+    if (!verifyResult) {
+      response.status(403).json({message: "Ошибка авторизации"})  
+    } else { 
+      let test = await User.findAll( {where: { id: verifyResult.userData }} );
+      test.fullName = updateName;
+      await User.update( test, {where: { id: verifyResult.userData }} ).then(res=> {
+      response.send({ result: res });    
+      });  
+    }
   }
 });
 app.delete('/delete', jsonParser, async (request, response)=> {
-  const { updateName } = request.body;
   const { authorization } = request.headers;
   const token = authorization.split(' ')[1];
   if (!token) {
     response.status(403).json({message: "Пользователь не авторизован"})  
   } else {  
-  const verifyResult = jwt.verify(token, secret);
-  if (!verifyResult) {
-    response.status(403).json({message: "Ошибка авторизации"})  
-  } else {  
-  await User.destroy( {where:{ fullName: updateName }} );
-  response.status(200).json({message: "Пользователь удален"})  
-  }
-  }
+    const verifyResult = jwt.verify(token, secret);
+    if (!verifyResult) {
+      response.status(403).json({message: "Ошибка авторизации"})  
+    } else {  
+        await User.destroy( {where:{ id: verifyResult.userData }} );
+        response.status(200).json({message: "Пользователь удален"})  
+      }
+    }
 });
 app.listen(3000, ()=> {
   console.log('сервер запущен')
