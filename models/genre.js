ed78efcd5140c7e08637a3947cbcf9ab611863e8
EvgeniyTTI <evgeniytti@rambler.ru>
2022-02-21 04:08:09 +0300

    fix migtation

--- models/genre.js
+++ models/genre.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      genre.hasMany(models.book);
       // define association here
     }
   }
