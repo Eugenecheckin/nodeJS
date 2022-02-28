580a250a407886cb9bb8d70fa43de7ec152448e2
EvgeniyTTI <evgeniytti@rambler.ru>
2022-02-28 16:38:08 +0300

    add token login

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -13,6 +13,6 @@ authRouter.post('/upload', isOwner, Storage, upload);
 authRouter.get('/test', test);
 authRouter.post('/signup', signUp);
 authRouter.post('/signin', signIn);
-authRouter.get('/login', isOwner, login);
+authRouter.post('/login', isOwner, login);
 
 export default authRouter;
