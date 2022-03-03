7c107af59bbe9927e0d671b40960331adea3c309
EvgeniyTTI <evgeniytti@rambler.ru>
2022-03-03 18:24:25 +0300

    add testing socketIo

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,37 @@
 const { PORT } = require('./config.ts');
 import app  from './app';
 
+import http  from 'http';
+const { Server } = require('socket.io');
 
-app.listen(PORT, () => {
-  console.log('сервер запущен');
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.use((socket:any, next:any) => {
+  const testMess = socket.handshake.auth.testMess;
+  if (!testMess) {
+    return next(new Error("invalid username"));
+  }
+  socket.testMess = testMess;
+  next();
+});
+
+io.on("connection", (socket:any) => {
+  // fetch existing users
+  const users = [];
+  for (let [ socket] of io.of("/").sockets) {
+    users.push({
+      testMess: socket.testMess,
+    });
+  }
+  socket.emit("users", users);
+
+  // notify existing users
+  socket.broadcast.emit("user connected", {
+    username: socket.testMess,
+  });
+});
+
+server.listen(PORT, () => {
+  console.log('сервер запущен '+PORT);
 });
